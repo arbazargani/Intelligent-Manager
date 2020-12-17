028a245e52e71e61041533b29171faac261c1ab4
Alireza Bazargani <arbazargani1998@gmail.com>
2020-12-18 01:47:14 +0330

    Application environments, Noindex, Git ignores.

--- resources/views/public/template.blade.php
+++ resources/views/public/template.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
     @yield('title')
     <!-- UIkit CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.16/dist/css/uikit.min.css" />
